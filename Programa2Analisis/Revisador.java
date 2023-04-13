5736da721abbe9e477767de2fc061dc2cdd39659
Alonsopro <alonso_togo@hotmail.com>
2023-04-13 14:52:32 -0600

    Agregue practica 2 de Analisis de algoritmos

---
+++ Programa2Analisis/Revisador.java
@@ -0,0 +1,11 @@
+//import Procesador;
+/**
+ * Revisador
+ */
+public class Revisador {
+
+    public static void main(String[] args) {
+        System.out.println("procesador");
+        Procesador p = new Procesador();
+    }
+}
